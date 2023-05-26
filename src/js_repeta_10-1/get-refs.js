451f56cf09fdbf030df717d5854d467835e0835b
Angey22 <babandval@gmail.com>
2023-05-26 05:51:50 +0300

    Added code & comments in files for first Repeta lection

--- src/js_repeta_10-1/get-refs.js
+++ src/js_repeta_10-1/get-refs.js
@@ -1,3 +1,4 @@
+// Пишем код функции, которая будет экспортировать объект с набором ссылок на элементы находящиеся в HTML документе.
 export default function getRefs() {
   return {
     cardContainer: document.querySelector('.js-card-container'),
